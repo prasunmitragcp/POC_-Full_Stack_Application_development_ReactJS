b3d6f2517ba2186df249d8c3c6f9833817f1836a
Prasun Mitra <pmitra@firstderivatives.com>
2022-04-07 23:39:12 +0100

    front end updated.

--- src/components/Allcourses.js
+++ src/components/Allcourses.js
@@ -31,7 +31,7 @@ const Allcourses=()=>{
             
         }
 
-         fetch('https://javamicroservices.uc.r.appspot.com/dbcollection').then(
+         fetch(`/dbcollection`).then(
              (response)=>{
                 //success
                 //console.log(response);
