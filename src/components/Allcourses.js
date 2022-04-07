63cbda056c56da847bf4bbe1847f7eb0736b016a
Prasun Mitra <pmitra@firstderivatives.com>
2022-04-07 10:09:07 +0100

    upload reactjs frontend

--- src/components/Allcourses.js
+++ src/components/Allcourses.js
@@ -31,7 +31,7 @@ const Allcourses=()=>{
             
         }
 
-         fetch(`https://javamicroservices.uc.r.appspot.com/dbcollection`).then(
+         fetch(`https://javamicroservices.uc.r.appspot.com/dbcollection`, {mode: 'no-cors'}).then(
              (response)=>{
                 //success
                 //console.log(response);
